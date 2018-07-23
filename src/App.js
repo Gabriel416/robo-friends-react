1c4e2ff8b352c417ac3dcde6b24b410bba514987
Gabriel416 <gferguson954@gmail.com>
2018-07-22 20:44:36 -0400

    robo friends 1.0

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
+import Scroll from "./Scroll";
 import "tachyons";
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      robots,
+      robots: [],
       search: ""
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => {
+        this.setState({ robots: users });
+      });
   }
 
   onSearchChange(event) {
@@ -20,16 +28,23 @@ class App extends React.Component {
     const filterdRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.search.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1>Robo Friends</h1>
-        <SearchBox
-          search={this.state.search}
-          searchChange={this.onSearchChange.bind(this)}
-        />
-        <CardList robots={filterdRobots} />
-      </div>
-    );
+
+    if (!this.state.robots) {
+      return <h3>Loading...</h3>;
+    } else {
+      return (
+        <div className="tc">
+          <h1>Robo Friends</h1>
+          <SearchBox
+            search={this.state.search}
+            searchChange={this.onSearchChange.bind(this)}
+          />
+          <Scroll>
+            <CardList robots={filterdRobots} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
